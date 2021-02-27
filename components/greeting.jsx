05dfbe78159ca6a4db958778c4b4cbc0078a0f14
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-02-27 16:51:25 -0500

    class final

--- components/greeting.jsx
+++ components/greeting.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react'
 
 export default () => {
-  const [slug, setSlug] = useState('')
+  const [person, setPerson] = useState('')
 
   return (
     <div className="page">
-      <div className="title">To Whom Am I Speaking?</div>
-      <div className="subtitle">A searchable list of all your favorite heroes</div>
-      <input type="text" name="search" onChange={event => setSlug(event.target.value)} />
-      <div className="slug">{slug}</div>
+      <div className="title">Who Am I Speaking With?</div>
+      <input type="text" name="search" onChange={event => setPerson(`It is nice to meet you ${event.target.value}`)} />
+      <div className="manu">{person}</div>
     </div>
   )
 }
